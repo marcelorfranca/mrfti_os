2dfc3e54ba9e04702dbaef5258d8b41b188b676c
Marcelo Ramos <marcelorfranca@hotmail.com>
2022-02-22 18:56:37 -0300

    3 - Criado UF e Municipios, Associacoes e JPA

---
+++ src/main/java/com/mrfti/erp/domain/Municipio.java
@@ -0,0 +1,81 @@
+package com.mrfti.erp.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Municipio implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+	protected String nome;
+	
+	@ManyToOne
+	@JoinColumn(name="uf_id")// definir nome da chave estrangeira na tabela municipio
+	protected Uf uf;
+	
+	public Municipio() {
+	}
+
+	public Municipio(Integer id, String nome, Uf uf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.uf = uf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Uf getUf() {
+		return uf;
+	}
+
+	public void setUf(Uf uf) {
+		this.uf = uf;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Municipio other = (Municipio) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+}
---
+++ src/main/java/com/mrfti/erp/domain/Uf.java
@@ -0,0 +1,91 @@
+package com.mrfti.erp.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Uf implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+	protected String nome;
+	protected String sigla;
+	
+	@OneToMany(mappedBy="uf")//informando qual foi o atributo que mapeou do lado de lá na classe Municipio
+	private List<Municipio> municipios = new ArrayList<>();
+	
+	public Uf() {
+	}
+
+	public Uf(Integer id, String nome, String sigla) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sigla = sigla;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
+	
+	public List<Municipio> getMunicipios() {
+		return municipios;
+	}
+
+	public void setMunicipios(List<Municipio> municipios) {
+		this.municipios = municipios;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Uf other = (Uf) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+}
